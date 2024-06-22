607924efe9947c5a2af8af93f53a140175d40c5c
Carlos Tomalino <carlostomalino@hotmail.com>
2024-06-22 12:01:11 -0300

    [upd] - remove comments

--- cliente-pgpio/Client2.py
+++ cliente-pgpio/Client2.py
@@ -1,7 +1,6 @@
 from tkgpio import TkCircuit
 from json import load
 
-# Traigo la configuración
 with open("Client2.json", "r") as file:
     configuration = load(file)
 
@@ -19,21 +18,3 @@ def main():
         print("apaga")
         led.off()
         sleep(1)
-
-    # def button2_pressed():
-    #     led2.toggle()
-    #
-    # # Creo Leds
-    # led1 = LED(21)
-    #
-    # # Creo Botones
-    # button1 = Button(11)
-    # button2 = Button(12)
-    #
-    # # Asigno eventos
-    # button1.when_pressed = led1.on
-    # button1.when_released = led1.off
-    # button2.when_pressed = button2_pressed
-    #
-    # while True:
-    #     sleep(0.1)
