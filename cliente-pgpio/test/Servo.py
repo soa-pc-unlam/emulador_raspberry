aa9f1dc6640c7160a419e4b0bb63b5b150ffdb7d
esteban <estebancarnuccio@gmail.com>
2024-07-30 16:40:20 -0300

    [U]add script to test servo, but not working

---
+++ cliente-pgpio/test/Servo.py
@@ -0,0 +1,29 @@
+from lib.gpiozero import Servo,Button
+from lib.tkgpio import TkCircuit
+from json import load
+from time import sleep
+
+
+with open("Servo.json", "r") as file:
+    configuration = load(file)
+
+circuit = TkCircuit(configuration)
+@circuit.run
+def main():
+
+
+    btnAngleInc= Button(12)
+
+    angle =0
+
+    while True:
+
+        if btnAngleInc.is_pressed:
+            angle += 0.20
+
+        Servo.value= angle
+
+        if angle == 1:
+            angle=0
+
+        sleep(0.1)
